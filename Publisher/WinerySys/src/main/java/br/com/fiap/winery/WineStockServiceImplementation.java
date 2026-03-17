9929702f63b416a0fac36bce44102031f14adfb6
jaoAprendiz <joaosoave94@gmail.com>
2026-03-17 20:17:38 -0300

    implementa metodo getMenu com lista de vinhos por tipo de uva

--- Publisher/WinerySys/src/main/java/br/com/fiap/winery/WineStockServiceImplementation.java
+++ Publisher/WinerySys/src/main/java/br/com/fiap/winery/WineStockServiceImplementation.java
@@ -1,4 +1,12 @@
 package br.com.fiap.winery;
 
 public class WineStockServiceImplementation {
+
+    public String getMenu() {
+        return "Menu de Vinhos:\n" +
+                "- Cabernet Sauvignon: Vinho Tinto Seco\n" +
+                "- Chardonnay: Vinho Branco Seco\n" +
+                "- Merlot: Vinho Tinto Suave\n" +
+                "- Pinot Noir: Vinho Tinto Leve";
+    }
 }

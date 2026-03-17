0e6aa672f48ef6502a9c037629fb00f6fbb42ce6
jaoAprendiz <joaosoave94@gmail.com>
2026-03-17 19:56:05 -0300

    adiciona anotacao @WebService na interface WineStockService

--- Publisher/WinerySys/src/main/java/br/com/fiap/winery/WineStockService.java
+++ Publisher/WinerySys/src/main/java/br/com/fiap/winery/WineStockService.java
@@ -1,5 +1,8 @@
 package br.com.fiap.winery;
 
+import jakarta.jws.WebService;
+
+@WebService
 public interface WineStockService {
     String getMenu();
     String placeOrder(String name, int quantity);

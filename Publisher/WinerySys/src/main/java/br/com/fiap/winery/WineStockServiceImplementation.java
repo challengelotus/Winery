c8cded2a01cb49b92f7ee4bdc623983f93c9c1d2
jaoAprendiz <joaosoave94@gmail.com>
2026-03-17 20:21:46 -0300

    implementa metodo placeOrder, adiciona anotacoes Override e corrige a classe WineStockServiceImplementation

--- Publisher/WinerySys/src/main/java/br/com/fiap/winery/WineStockServiceImplementation.java
+++ Publisher/WinerySys/src/main/java/br/com/fiap/winery/WineStockServiceImplementation.java
@@ -1,12 +1,18 @@
 package br.com.fiap.winery;
 
-public class WineStockServiceImplementation {
+public class WineStockServiceImplementation implements WineStockService {
 
+    @Override
     public String getMenu() {
         return "Menu de Vinhos:\n" +
                 "- Cabernet Sauvignon: Vinho Tinto Seco\n" +
                 "- Chardonnay: Vinho Branco Seco\n" +
                 "- Merlot: Vinho Tinto Suave\n" +
                 "- Pinot Noir: Vinho Tinto Leve";
+    }
+
+    @Override
+    public String placeOrder(String name, int quantity) {
+        return "Pedido confirmado!";
     }
 }

65ad3eb0dc0cba00a45c894027e5135c8294118f
Malice112 <maliceefa@gmail.com>
2026-03-17 21:10:58 -0300

    adicionando anotação webservice e configurando endpointInterface

--- Publisher/WinerySys/src/main/java/br/com/fiap/winery/WineStockServiceImplementation.java
+++ Publisher/WinerySys/src/main/java/br/com/fiap/winery/WineStockServiceImplementation.java
@@ -1,5 +1,9 @@
 package br.com.fiap.winery;
 
+
+import jakarta.jws.WebService;
+
+@WebService(endpointInterface = "br.com.fiap.winery.WineStockService")
 public class WineStockServiceImplementation implements WineStockService {
 
     @Override

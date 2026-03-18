6b00f83952b4780643da9eb84bdcc727838c32fa
Malice112 <maliceefa@gmail.com>
2026-03-17 21:18:30 -0300

    instaciando WineStockServiceImplementation na classse Loader

--- Publisher/WinerySys/src/main/java/br/com/fiap/winery/Loader.java
+++ Publisher/WinerySys/src/main/java/br/com/fiap/winery/Loader.java
@@ -1,4 +1,7 @@
 package br.com.fiap.winery;
 
 public class Loader {
+    public static void main(String[] args) {
+        WineStockServiceImplementation wineStock = new WineStockServiceImplementation();
+    }
 }
